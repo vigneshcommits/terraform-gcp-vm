7756842dc251cca3a34a52edf3c1995d19100e10
vignesh v <vignesh@calyptia.com>
2023-03-09 12:37:24 +0530

    initial commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "image_name" {
+    default = "centos-7-v20200403"
+    type = string
+}
+
+variable "machine_type" {
+    default = "e2-medium"
+    type = string
+}
+
+variable "zone" {
+    default = "us-east1-b"
+    type = string
+}
